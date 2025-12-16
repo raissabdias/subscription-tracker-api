582b68ca68780a8515a467ac1bdfcb81c659e1a6
Raíssa Dias <raissablw@gmail.com>
2025-12-16 19:25:35 -0300

    feat(front-end): instalação do vue e vue router

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="font-family: sans-serif; text-align: center; padding: 50px;">
+        <h1>Olá, Vue 3 no Laravel! 🚀</h1>
+        <p>Se você está vendo isso, o front-end funcionou.</p>
+        
+        <button @click="count++">Cliques: {{ count }}</button>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const count = ref(0);
+</script>
